9cbe28809330312bf75b056665bf335b4a1698ab
Andrey Paramonov <git@ndpar.com>
2011-05-19 08:02:43 -0400

    Exercise 1.23

--- sicp.scm
+++ sicp.scm
@@ -33,9 +33,11 @@
   (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
+  (define (next td)
+    (if (= td 2) 3 (+ td 2)))
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
